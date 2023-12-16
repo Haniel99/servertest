07570b0fe50c6cbc958a11e2a1cabb980f5bf05e
AnibalN25 <104791894+Haniel99@users.noreply.github.com>
2023-12-16 17:22:03 -0300

    Segunt commmit

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize("g_practicas", "root", "nicol225", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-export default sequelize;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import sequelize from './config';
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app: Application = express();
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('¡Hola, mundo!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("¡Hola, mundo!");
 });
 
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor Express iniciado en el puerto ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Servidor Express iniciado en el puerto ${PORT}`);
 });
